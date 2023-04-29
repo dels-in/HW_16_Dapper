336cb4d5575435629c54d4892275e395a85e563e
Vlad Chernenko <107535867+dels-in@users.noreply.github.com>
2023-04-29 21:20:08 +0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,6 @@
+namespace HW_16;
+
+public static class Config
+{
+      public static string SqlConnectionString = "User ID=postgres;Password=Vlad3954;Host=localhost;Port=5432;Database=Shop;";
+}
---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+namespace HW_16.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int StockQuantity { get; set; }
+    public int Price { get; set; }
+}
